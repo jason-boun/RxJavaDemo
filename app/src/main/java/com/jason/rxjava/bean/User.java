27c1af55cbf3a8b735824bea8070a0d7137395e9
jason-boun <jasonboun@163.com>
2017-10-15 16:35:08 +0800

    json format 插件测试

---
+++ app/src/main/java/com/jason/rxjava/bean/User.java
@@ -0,0 +1,42 @@
+package com.jason.rxjava.bean;
+
+/**
+ * Created by JiaBo on 2017/10/15.
+ */
+
+public class User {
+
+    /**
+     * name : jason
+     * age : 23
+     * grade : 6
+     */
+
+    private String name;
+    private int age;
+    private int grade;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+}
